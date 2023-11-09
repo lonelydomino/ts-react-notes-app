8b830877e603f66ed5c0d7716f4091345fe43eb5
lonelydomino <lonelydomino@icloud.com>
2023-11-08 17:23:38 -0800

    first commit

---
+++ notes-app/notes-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Routes, Route } from 'react-router-dom'
+function App() {
+
+  return (
+    <>
+     <Routes>
+     </Routes>
+    </>
+  )
+}
+
+export default App
